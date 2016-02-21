e1ff176d42febdf431b7500dd6c730b2efa043c3
amiel asparen <amielasparen@gmail.com>
2016-02-21 14:31:18 +0800

    a

--- server.js
+++ server.js
@@ -2,4 +2,5 @@
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+connect().use(serveStatic(__dirname)).listen(8000);	
+var port = process.env.PORT || 8000;
